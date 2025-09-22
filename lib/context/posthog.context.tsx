fcfe4ec4608704c296c442f62e2907ad5aad221c
Neil Shah <neil@bronze.app>
2025-09-22 16:12:45 -0400

    Capture page views

--- lib/context/posthog.context.tsx
+++ lib/context/posthog.context.tsx
@@ -11,7 +11,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
         api_host: '/ingest',
         ui_host: 'https://us.posthog.com',
-        capture_pageview: false,
+        capture_pageview: true,
         capture_pageleave: true,
       });
     }
